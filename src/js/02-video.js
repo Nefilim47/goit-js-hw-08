7ea820c0a35c1207cd091044f77870fe75ddda78
Nefilim47 <65806868+Nefilim47@users.noreply.github.com>
2023-04-23 14:12:45 +0300

    2-nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoEl = document.querySelector("#vimeo-player")
+const player = new Player(videoEl);
+
+if (localStorage.getItem("videoplayer-current-time")) {  
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+}
+
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}; 
+
+player.on("timeupdate", throttle(onPlay, 1000));
