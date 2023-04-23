60cef9319fcbf0810facfe122aecc05c2e103751
Nefilim47 <65806868+Nefilim47@users.noreply.github.com>
2023-04-23 12:30:43 +0300

    01-st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createImg = galleryItems.map((el) => {
+  const { preview, original, description } = el;
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        loading="lazy"/>
+    </a>
+  </li>`;
+});
+
+galleryEl.insertAdjacentHTML("beforeend", createImg.join(""));
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
