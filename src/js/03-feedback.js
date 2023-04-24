9c88516a4151230917e151cf254b0b0a31c7046f
Nefilim47 <65806868+Nefilim47@users.noreply.github.com>
2023-04-24 20:58:21 +0300

    fix - 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,38 +7,37 @@ const messageInputEl = form.querySelector('textarea[name="message"]');
 const storageKey = 'feedback-form-state';
 const localStorageThrottle = throttle(saveLocalStorage, 500);
 
-
 function saveLocalStorage () {
-  const state = {
+    const state = {
     email: emailInputEl.value,
     message: messageInputEl.value,
   };
   localStorage.setItem(storageKey, JSON.stringify(state));
 };
-const onPageReload = () => {
-    const state = JSON.parse(localStorage.getItem(storageKey));
-    console.log(state)
-    if (state) {
-        emailInputEl.value = state.emailInputEl;
-        messageInputEl.value = state.messageInputEl;
-    };
-};
-onPageReload();
+
 
 function onFormSubmit(e) {
     e.preventDefault();
     console.log({ email: emailInputEl.value, message: messageInputEl.value });
-  
-    if (emailInputEl.value === '' || messageInputEl.value === '') {
+      if (emailInputEl.value === '' || messageInputEl.value === '') {
       return alert('Введите данные');
     }  
-    localStorage.removeItem(storageKey);
     e.currentTarget.reset();
-    dataForm = {};
+    state = {};
   }
+
+  const onPageReload = () => {
+    const state = JSON.parse(localStorage.getItem(storageKey));
+    // console.log(state)
+    if (state) {
+        emailInputEl.value = state.email;
+        messageInputEl.value = state.message ;
+    };
+};
 
 form.addEventListener('input', localStorageThrottle);
 form.addEventListener('submit', onFormSubmit)
+onPageReload();
 
 
 
