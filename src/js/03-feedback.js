5b6145794451d6045299be445bc7755062cce84e
Nefilim47 <65806868+Nefilim47@users.noreply.github.com>
2023-04-24 20:32:26 +0300

    third

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const emailInputEl = form.querySelector('input[name="email"]');
+const messageInputEl = form.querySelector('textarea[name="message"]');
+
+const storageKey = 'feedback-form-state';
+const localStorageThrottle = throttle(saveLocalStorage, 500);
+
+
+function saveLocalStorage () {
+  const state = {
+    email: emailInputEl.value,
+    message: messageInputEl.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(state));
+};
+const onPageReload = () => {
+    const state = JSON.parse(localStorage.getItem(storageKey));
+    console.log(state)
+    if (state) {
+        emailInputEl.value = state.emailInputEl;
+        messageInputEl.value = state.messageInputEl;
+    };
+};
+onPageReload();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log({ email: emailInputEl.value, message: messageInputEl.value });
+  
+    if (emailInputEl.value === '' || messageInputEl.value === '') {
+      return alert('Введите данные');
+    }  
+    localStorage.removeItem(storageKey);
+    e.currentTarget.reset();
+    dataForm = {};
+  }
+
+form.addEventListener('input', localStorageThrottle);
+form.addEventListener('submit', onFormSubmit)
+
+
 
